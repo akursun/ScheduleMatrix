bd42f188736a95651716bb52e655b701d949a2d7
aliosmankursun@hotmail.com <CodingMastery202!>
2021-11-17 15:16:33 -0600

    OutPut Files are created with students' name in them.

--- src/test/java/utils/ExcelUtilsTest.java
+++ src/test/java/utils/ExcelUtilsTest.java
@@ -1,5 +1,11 @@
 package utils;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class ExcelUtilsTest {
@@ -24,9 +30,25 @@ public class ExcelUtilsTest {
                 System.out.println(student.grade);
                 System.out.println(student.lockerCombination);
                 System.out.println("----------");
+                //TODO Get student's classes.
+
+                XSSFWorkbook workbookSc = new XSSFWorkbook();
+                XSSFSheet sheetSc = workbookSc.createSheet(student.name+"'s Schedule");
+                Row rowSc= sheetSc.createRow(4);
+                Cell cellSc = rowSc.createCell(2);
+                cellSc.setCellValue(student.name);
+                FileOutputStream fileout = new FileOutputStream("/Users/aliokursun/IdeaProjects/Excel/OutputFiles/"+student.name+"'s Schedule.xlsx");
+                workbookSc.write(fileout);
+                fileout.flush();
+                fileout.close();
+
+
             }
+
         }
 
 
     }
+
+
 }
